111384b27846d3f29eadd6c965a2e6131ac973a4
Unknown <t4trjo00@students.oamk.fi>
2016-10-31 10:33:37 +0200

    post lomake valmis

---
+++ post.php
@@ -0,0 +1,18 @@
+<h1>Testataan post-metodia</h1>
+<form method="post" action="post.php">
+<label>Etunimi	</label>
+<input type="text" name="en">
+<br>
+<label>Sukunimi	</label>
+<input type="text" name="sn">
+<br>
+<input type="submit" name="nappi" value="Lähetä">
+</form>
+
+<?php
+if(isset($_POST['nappi'])){
+	$enimi=$_POST['en'];
+	$snimi=$_POST['sn'];
+	echo "terve $enimi $snimi";
+}
+?>
